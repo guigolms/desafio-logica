fa7e2c5f02f40a7038166703771d012f93da4b18
guigolms <guigolms@hotmail.com>
2024-08-22 20:13:42 -0300

    finalizado

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log("foda meu mano")
+let nome = "Felipe";
+let experiencia = 10000;
+
+if (experiencia <= 1000){
+    console.log("O Herói de nome "+nome+" está no nível de Ferro")
+} 
+else if (experiencia > 1000 && experiencia <= 2000){
+    console.log("O Herói de nome "+nome+" está no nível de Bronze")
+}
+else if (experiencia > 2000 && experiencia <= 5000){
+    console.log("O Herói de nome "+nome+" está no nível de Prata")
+}
+else if (experiencia > 5000 && experiencia <= 7000){
+    console.log("O Herói de nome "+nome+" está no nível de Ouro")
+}
+else if (experiencia > 7000 && experiencia <= 8000){
+    console.log("O Herói de nome "+nome+" está no nível de Platina")
+}
+else if (experiencia > 8000 && experiencia <= 9000){
+    console.log("O Herói de nome "+nome+" está no nível de Ascendente")
+}
+else if (experiencia > 9000 && experiencia <= 10000){
+    console.log("O Herói de nome "+nome+" está no nível de Imortal")
+}
+else if (experiencia > 10000){
+    console.log("O Herói de nome "+nome+" está no nível de Radiante")
+}
